867b2c3228436752f443255da9791a75b9a1e310
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-07-07 08:45:01 +0530

    Add files via upload

---
+++ AutoBox.java
@@ -0,0 +1,49 @@
+public class AutoBox {
+public static void main(String[] args){
+    byte b=10;
+    Byte B=b; //AutoBox
+    byte bb=B;   ///Unboxing
+
+    short s=20;
+    Short S=s;
+    short ss=S;
+
+    int i=30;
+    Integer I=i;
+    int ii=I;
+
+    long l=40L;
+    Long L=l;
+    long ll=L;
+
+    float f=3.14f;
+    Float F=f;
+    float ff=F;
+
+    double d=6.28;
+    Double D=d;
+    double dd=D;
+
+    char c='A';
+    Character C=c;
+    char cc=C;
+
+    boolean bol=true;
+    Boolean Bol=bol;
+    boolean bb1=Bol;
+
+    System.out.println("AutoBoxing and Unboking is done uccessfully");
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+}
