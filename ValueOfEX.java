abd3d56b6429b92c099d98dadc375933792b2e6b
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-07-07 09:44:53 +0530

    Add files via upload

---
+++ ValueOfEX.java
@@ -0,0 +1,26 @@
+public class ValueOfEX {
+    public static void main(String[] args){
+        Byte b=Byte.valueOf("10");
+        Short s=Short.valueOf("20");
+        Integer i=Integer.valueOf("123");
+        Long l=Long.valueOf("45678");
+        Float f=Float.valueOf("3.45");
+        Double d=Double.valueOf("8.9876");
+        Boolean bool=Boolean.valueOf("true");
+        Character c=Character.valueOf('A');
+
+        System.out.println("Byte: "+b);
+        System.out.println("Short: "+s);
+        System.out.println("Integer: "+i);
+        System.out.println("Long: "+l);
+        System.out.println("Float: "+f);
+        System.out.println("Double: "+d);
+        System.out.println("Boolean: "+b);
+        System.out.println("Character: "+c);
+
+
+
+
+
+    }
+}
